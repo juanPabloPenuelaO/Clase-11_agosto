d35d0a0d2a1e24d09e10c30ebb1d6e03916b88ce
juanPabloPenuelaO <juanppenuelaocampo@gmail.com>
2023-08-27 16:43:10 -0500

    clase 11 Agosto

---
+++ src/Agregacion2/Factura.java
@@ -0,0 +1,13 @@
+package Agregacion2;
+
+public class Factura {
+    private String NumeroFactura;
+    private String fecha;
+    Producto producto;
+
+    public Factura(String numeroFactura, String fecha, Producto producto) {
+        NumeroFactura = numeroFactura;
+        this.fecha = fecha;
+        this.producto = producto;
+    }
+}
---
+++ src/Agregacion2/Producto.java
@@ -0,0 +1,27 @@
+package Agregacion2;
+
+public class Producto {
+    private String NombreProducto;
+    private int valor;
+
+    public Producto(String nombreProducto, int valor) {
+        NombreProducto = nombreProducto;
+        this.valor = valor;
+    }
+
+    public String getNombreProducto() {
+        return NombreProducto;
+    }
+
+    public void setNombreProducto(String nombreProducto) {
+        NombreProducto = nombreProducto;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+}
